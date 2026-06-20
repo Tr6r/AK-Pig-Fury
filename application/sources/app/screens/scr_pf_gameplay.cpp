e252e2078eeb00bb078db03dac663f5db8587db4
Dinh Quoc Cuong <tr6r20@gmail.com>
2026-06-21 00:01:19 +0700

    scr: update gameplay scr - render pig atk when button was pressed

--- application/sources/app/screens/scr_pf_gameplay.cpp
+++ application/sources/app/screens/scr_pf_gameplay.cpp
@@ -23,11 +23,28 @@ void view_scr_pf_gameplay() {
 
 void scr_pf_gameplay_handle(ak_msg_t *msg) {
 	switch (msg->sig) {
-	case SCREEN_ENTRY: {
-		APP_DBG_SIG("PF_GAMEPLAY\n");
-		task_post_pure_msg(AC_TASK_PF_GAME_ID, AC_PF_GAME_INIT);
-	} break;
-	default:
-		break;
+		case SCREEN_ENTRY: {
+			APP_DBG_SIG("PF_GAMEPLAY\n");
+			task_post_pure_msg(AC_TASK_PF_GAME_ID, AC_PF_GAME_INIT);
+			timer_set(AC_TASK_DISPLAY_ID,AC_DISPLAY_GAMEPLAY_UPDATE,AC_DISPLAY_GAMEPLAY_UPDATE_INTERVAL_MS,TIMER_PERIODIC);
+			break;
+		}
+		case AC_DISPLAY_GAMEPLAY_UPDATE: {
+			scr_mng_invalidate();
+			pf_game_update();
+			break;
+		}
+		case AC_DISPLAY_BUTON_UP_PRESSED: {
+			pf_char_dir dir = RIGHT;
+			task_post_common_msg(AC_TASK_PF_GAME_ID, AC_PF_PIG_ATK, (uint8_t*)&dir, sizeof(dir));
+			break;
+		}
+		case AC_DISPLAY_BUTON_DOWN_PRESSED: {
+			pf_char_dir dir = LEFT;
+			task_post_common_msg(AC_TASK_PF_GAME_ID, AC_PF_PIG_ATK, (uint8_t*)&dir, sizeof(dir));
+			break;
+		}
+		default:
+			break;
 	}
 }
