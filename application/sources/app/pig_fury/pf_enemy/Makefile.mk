55d1600867a5655ab2bbe18d239625c49937a6e5
Dinh Quoc Cuong <tr6r20@gmail.com>
2026-06-28 14:07:10 +0700

    game: change dir path name

--- application/sources/app/pig_fury/pf_enemy/Makefile.mk
+++ application/sources/app/pig_fury/pf_enemy/Makefile.mk
@@ -2,7 +2,6 @@ CFLAGS		+= -I./sources/app/pig_fury/pf_enemy
 CPPFLAGS	+= -I./sources/app/pig_fury/pf_enemy
 
 VPATH += sources/app/pig_fury/pf_enemy
-
-DIR = sources/app/pig_fury/pf_enemy
+PF_ENEMY_DIR := sources/app/pig_fury/pf_enemy
 # CPP source files
-SOURCES_CPP +=	$(DIR)/pf_wolf_basic.cpp	\
+SOURCES_CPP +=	$(PF_ENEMY_DIR)/pf_wolf_basic.cpp \
